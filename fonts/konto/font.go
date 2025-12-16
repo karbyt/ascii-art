b32632b45087edda011d299391a4f3d42e726355
karbyt <ayakaneesan@gmail.com>
2025-12-16 20:07:49 +0700

    Add fonts

---
+++ fonts/konto/font.go
@@ -0,0 +1,14 @@
+package asciiart
+
+import (
+	_ "embed"
+
+	"github.com/romance-dev/ascii-art"
+)
+
+//go:embed font.flf
+var font []byte
+
+func init() {
+	asciiart.RegisterFont("konto", font)
+}
